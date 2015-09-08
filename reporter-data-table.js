846f89c294463c67211ee30c6af90b9c3e70f42a
Harry Ward <harryward@townsquaremedia.com>
2015-09-08 08:46:24 -0600

    reporter

---
+++ reporter-data-table.js
@@ -0,0 +1,9 @@
+
+Template.reportDataTable.helpers({
+    columnHeaders: function () {
+        return Reporter.columnHeaders();
+    },
+    rows: function () {
+        return Reporter.rows();
+    }
+});
